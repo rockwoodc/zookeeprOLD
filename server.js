f70d56d908cf826861fa2280e6fc1f94008ef6cc
rockwoodc <rockwoodce@gmail.com>
2022-08-03 15:46:00 -0400

    added validations and adding animal function

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 //will use the port the application suggests
 const PORT = process.env.PORT || 3001;
 //tells server to listen for requests
 const app = express();
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 //allows access to animal data
 const { animals } = require('./data/animals.json');
 //add the route
@@ -50,7 +58,19 @@ function filterByQuery(query, animalsArray) {
 function findById(id, animalsArray) {
     const result = animalsArray.filter(animal => animal.id === id)[0];
     return result;
-  }
+  };
+//when we POST a new animal, we'll add it to the imported animals array from the animals.json file
+function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+        path.join(__dirname, './data/animals.json'),
+        JSON.stringify({ animals: animalsArray }, null, 2)
+      );
+    
+    //return finished code to post route for response
+    return animal;
+}
 
 //used when we only want one specific animal, rather than an array of all the animals that match a query.
 app.get('/api/animals/:id', (req, res) => {
@@ -70,6 +90,37 @@ app.get('/api/animals', (req, res) => {
         }
     res.json(results);
 });
+
+//post requests represent the action of a client requesting the server to accept data
+app.post('/api/animals', (req, res) => {
+    //set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+        res.status(400).send('The animal is not properly formatted.');
+    } else {
+    // add animal to json file and animals array in this function
+     const animal = createNewAnimal(req.body, animals);
+
+    res.json(animal);
+    }
+});
+
+function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+    }
+    return true;
+  }
 
 //method to make the server listen
 app.listen(PORT, () => {
